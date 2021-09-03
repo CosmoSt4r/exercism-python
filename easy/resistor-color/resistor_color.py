b2832f523825dbc9e173be5a5b880c6bf0e697bd
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-03 11:30:45 +0500

    Resist-color task solved

---
+++ easy/resistor-color/resistor_color.py
@@ -0,0 +1,20 @@
+colors_values = (
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+)
+
+
+def color_code(color):
+    return colors_values.index(color)
+
+
+def colors():
+    return list(colors_values)
