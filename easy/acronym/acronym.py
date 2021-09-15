52a95024bd98d316b9e56084cc672f3ea9f356bd
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-15 16:22:16 +0500

    Acronym task formatted with flake8 linter

--- easy/acronym/acronym.py
+++ easy/acronym/acronym.py
@@ -1,3 +1,19 @@
-def abbreviate(words):
-    words = words.replace("_", " ").replace(",", " ").replace("-", " ")
-    return "".join(word[0].upper() for word in words.split())
+"""
+Solution to Acronym task on Exercism.
+
+https://exercism.org/tracks/python/exercises/acronym
+"""
+import re
+
+
+def abbreviate(phrase: str) -> str:
+    """Convert a phrase to its acronym.
+
+    Args:
+        phrase: string with words
+
+    Returns:
+        acronym for given phrase
+    """
+    words: list = re.sub(r'[_+\,+\-+]', ' ', phrase).split()
+    return ''.join(word[0].upper() for word in words)
