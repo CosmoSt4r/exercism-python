59814f09bdd99a89cd877ce82a42a4d1d8d81355
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-20 20:26:24 +0500

    Allergies task styled for flake8

--- easy/allergies/allergies.py
+++ easy/allergies/allergies.py
@@ -1,41 +1,75 @@
 """
-Solution to Allergies task on Exercism
+Solution to Allergies task on Exercism.
 
 https://exercism.org/tracks/python/exercises/allergies
 """
 
-class Allergies:
-    """Allergies class"""
+
+class Allergies(object):
+    """Allergies class."""
 
     allergies = (
-        'eggs', 'peanuts', 'shellfish', 'strawberries',
-        'tomatoes', 'chocolate', 'pollen', 'cats',
-        )
+        'eggs',
+        'peanuts',
+        'shellfish',
+        'strawberries',
+        'tomatoes',
+        'chocolate',
+        'pollen',
+        'cats',
+    )
 
     def __init__(self, score: int):
+        """
+        Initialize Allergies object.
+
+        Args:
+            score (int): allergies score.
+
+        """
+        self.max_score = 256
         self.lst = score
 
-    def allergic_to(self, item: int) -> bool:
-        """Return True if person is allergic to given item"""
+    def allergic_to(self, allergen: int) -> bool:
+        """
+        Check if person is allergic to given allergen.
 
-        return item in self._lst
+        Args:
+            allergen (int): object that can cause allergy.
+
+        Returns:
+            bool: true if person is allergic to given allergen.
+
+        """
+        return allergen in self.lst
 
     @property
     def lst(self) -> list:
-        """Get list of allergies"""
+        """
+        Get list of allergies.
 
+        Returns:
+            list: list of allergies person is allergic to.
+
+        """
         return self._lst
 
     @lst.setter
     def lst(self, score: int) -> None:
-        """Set list of all allergies a person have based on score"""
+        """
+        Set list of allergies from person's allergy score.
 
+        Args:
+            score (int): person's allergy score.
+
+        """
         self._lst: list = []
-        score %= 256
+        score %= self.max_score
 
-        for value, index in ((2**x, x) for x in range(7, -1, -1)):
-            if value <= score:
-                score -= value
+        for index in range(7, -1, -1):
+            allergen_value = 2**index
+            if allergen_value <= score:
+                score -= allergen_value
                 self._lst.append(self.allergies[index])
             if score == 0:
                 break
