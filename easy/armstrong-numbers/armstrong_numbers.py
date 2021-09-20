a9d81a5a6e5bc134a2dc8bab0f9f2ad00c8af23e
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-20 22:00:28 +0500

    Armstrong numbers task styled to flake8

--- easy/armstrong-numbers/armstrong_numbers.py
+++ easy/armstrong-numbers/armstrong_numbers.py
@@ -1,15 +1,44 @@
 """
-Solution for Armstrong Numbers task on Exercism
+Solution for Armstrong Numbers task on Exercism.
 
 https://exercism.org/tracks/python/exercises/armstrong-numbers
 """
 
 
-def is_armstrong_number(number):
+def get_armstrong_value(number: int) -> int:
     """
-    Determine whether the number is Armstrong number or not
+    Get Armstrong number of given number.
+
+    Armstrong value of a number is the sum of its digits
+    each raised to the power of the number of its digits.
+
+    Args:
+        number (int): input number.
+
+    Returns:
+        int: Armstrong value of given number.
+
+    """
+    armstrong_value: list = []
+    number_len: int = len(str(number))
+
+    for digit in str(number):
+        armstrong_value.append(int(digit) ** number_len)
+    return sum(armstrong_value)
+
+
+def is_armstrong_number(number: int) -> bool:
+    """
+    Determine whether the number is Armstrong number or not.
 
     An Armstrong number is a number that is the sum of its
     own digits each raised to the power of the number of digits
+
+    Args:
+        number (int): number to check.
+
+    Returns:
+        bool: True if number is Armstrong number.
+
     """
-    return sum([int(x) ** len(str(number)) for x in str(number)]) == number
+    return get_armstrong_value(number) == number
