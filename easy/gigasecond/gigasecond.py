353966afc3f51771b33e336d30888a36b4216c01
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-22 20:49:42 +0500

    Gigasecond task styled to flake8

--- easy/gigasecond/gigasecond.py
+++ easy/gigasecond/gigasecond.py
@@ -1,6 +1,21 @@
+"""
+Solution to Gigasecond task on Exercism.
+
+https://exercism.org/tracks/python/exercises/gigasecond
+"""
+
 from datetime import timedelta
 
 
 def add(moment):
-    """Return the moment that would be after a gigasecond has passed"""
+    """
+    Get the moment that would be after a gigasecond has passed.
+
+    Args:
+        moment: current datetime.
+
+    Returns:
+        datetime after a gigasecond has passed.
+
+    """
     return moment + timedelta(seconds=10 ** 9)
