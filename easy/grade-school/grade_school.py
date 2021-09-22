40efc9ddb77b48c2ffbc6ef233116307d8cbad3c
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-22 21:12:41 +0500

    Grade School task styled to flake8

--- easy/grade-school/grade_school.py
+++ easy/grade-school/grade_school.py
@@ -1,30 +1,92 @@
+"""
+Solution to Grade School task on Exercism.
+
+https://exercism.org/tracks/python/exercises/grade-school
+"""
+
+
+from __future__ import annotations
+
 from functools import cmp_to_key
 
 
-class Student:
-    def __init__(self, name, grade):
-        self.name = name
-        self.grade = grade
+def sort_students(first: Student, second: Student) -> int:
+    """
+    Compare two Student objects.
 
-    @staticmethod
-    def sort(first, second):
-        if first.grade == second.grade:
-            return 1 if second.name < first.name else -1
-        return 1 if second.grade < first.grade else -1
+    Args:
+        first (Student): first student to compare.
+        second (Student): second student to compare.
+
+    Returns:
+        int: 1 if first > second else -1
+
+    """
+    if first.grade == second.grade:
+        return 1 if second.name < first.name else -1
+    return 1 if second.grade < first.grade else -1
 
 
-class School:
+class Student(object):
+    """Class for students."""
+
+    def __init__(self, name: str, grade: int):
+        """
+        Initialize Student object.
+
+        Args:
+            name (str): student's name.
+            grade (int): student's grade.
+
+        """
+        self.name: str = name
+        self.grade: int = grade
+
+
+class School(object):
+    """Class for school."""
+
     def __init__(self):
-        self.students = []
+        """Initialize School object."""
+        self.students: list = []
 
-    def add_student(self, name, grade):
+    def add_student(self, name: str, grade: int) -> None:
+        """
+        Add student to school.
+
+        Adds student to students' list and sorts it.
+
+        Args:
+            name (str): student's name.
+            grade (int): student's grade.
+
+        """
         self.students.append(Student(name, grade))
-        self.students.sort(key=cmp_to_key(Student.sort))
+        self.students.sort(key=cmp_to_key(sort_students))
 
-    def roster(self):
+    def roster(self) -> list:
+        """
+        Get list with students' names.
+
+        Returns:
+            list: list with students' names.
+
+        """
         return [student.name for student in self.students]
 
-    def grade(self, grade_number):
+    def grade(self, grade_number: int) -> list:
+        """
+        Get all students with specified grade.
+
+        Args:
+            grade_number (int): student's grade.
+
+        Returns:
+            list: list with students' names with specified grades.
+
+        """
         return [
-            student.name for student in self.students if student.grade == grade_number
+            student.name
+            for student in self.students
+            if student.grade == grade_number
         ]
