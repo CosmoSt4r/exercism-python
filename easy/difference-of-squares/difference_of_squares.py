dee992f2feb480a1b48862b5d00c98bf50b26e26
Andrey Kalkutin <CosmoStar@protonmail.com>
2021-09-22 20:44:07 +0500

    Diff of Squares task styled to flake8

--- easy/difference-of-squares/difference_of_squares.py
+++ easy/difference-of-squares/difference_of_squares.py
@@ -1,26 +1,53 @@
 """
-Solution to Difference of Squares task on Exercism
+Solution to Difference of Squares task on Exercism.
 
 https://exercism.org/tracks/python/exercises/difference-of-squares
 """
 
-def square_of_sum(number):
-    """Return square of sum of the first N numbers"""
+
+def square_of_sum(number: int) -> int:
+    """
+    Get square of sum of the first N numbers.
+
+    Args:
+        number (int): limit.
+
+    Returns:
+        int: square of sum of the first N numbers.
+
+    """
     return sum(range(1, number + 1))**2
 
 
-def sum_of_squares(number):
-    """Return sum of squares of the first N numbers"""
-    return sum([i**2 for i in range(1, number + 1)])
-
-
-def difference_of_squares(number):
+def sum_of_squares(number: int) -> int:
     """
-    Return the difference between square of sum and
-    sum of squares of the first N numbers
-    """
+    Get sum of squares of the first N numbers.
 
+    Args:
+        number (int): limit.
+
+    Returns:
+        int: sum of squares of the first N numbers.
+
+    """
+    return sum([num**2 for num in range(1, number + 1)])
+
+
+def difference_of_squares(number: int) -> int:
+    """
+    Get the difference between square of sum and sum of squares.
+
+    Args:
+        number (int): limit.
+
+    Returns:
+        int: difference between square of sum and sum of squares.
+
+    Raises:
+        ValueError: if number less than or equal to zero.
+
+    """
     if number <= 0:
-        raise ValueError("Number can not be less than or equal to zero")
+        raise ValueError('Number can not be less than or equal to zero')
 
     return square_of_sum(number) - sum_of_squares(number)
